8cb9097d9e8df2c7c58c58220217f962a5354362
LensPlatform <iinvestplatform@gmail.com>
2019-10-31 23:53:18 -0700

    Added endpoint, http, service

---
+++ pkg/api/service/middleware.go
@@ -0,0 +1,154 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	_ "github.com/go-kit/kit/log"
+	"go.uber.org/zap"
+)
+
+type Middleware func(UsersService) UsersService
+
+type loggingMiddleware struct {
+	logger *zap.Logger
+	next   UsersService
+}
+
+func LoggingMiddleware(logger *zap.Logger) Middleware {
+	return func(next UsersService) UsersService {
+		return &loggingMiddleware{logger, next}
+	}
+}
+
+func (l loggingMiddleware) CreateUserAccount(ctx context.Context, user interface{}, userType string) (id string, e0 error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "CreateUserAccount"), zap.String("user", fmt.Sprintf("%v", user)),
+			zap.String("userType", userType), zap.Any("err", e0))
+	}()
+	return l.next.CreateUserAccount(ctx, user, userType)
+}
+func (l loggingMiddleware) UpdateUserAccount(ctx context.Context, user interface{}, id string) (userID string, e0 error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "UpdateUserAccount"), zap.String("user", fmt.Sprintf("%v", user)),
+			zap.String("id", id), zap.Any("err", e0))
+	}()
+	return l.next.UpdateUserAccount(ctx, user, id)
+}
+func (l loggingMiddleware) DeleteUserAccount(ctx context.Context, id string) (userId string, e0 error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "DeleteUserAccount"),
+			zap.String("id", id), zap.Any("err", e0))
+	}()
+	return l.next.DeleteUserAccount(ctx, id)
+}
+func (l loggingMiddleware) GetUserAccount(ctx context.Context, id string) (user interface{}, err error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "GetUserAccount"), zap.String("user", fmt.Sprintf("%v", user)),
+			zap.String("id", id), zap.Any("err", err))
+	}()
+	return l.next.GetUserAccount(ctx, id)
+}
+func (l loggingMiddleware) CreateTeamsAccount(ctx context.Context, team interface{}, teamType string) (id string, e0 error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "CreateTeamsAccount"), zap.String("team", fmt.Sprintf("%v", team)),
+			zap.String("teamType", teamType), zap.Any("err", e0))
+	}()
+	return l.next.CreateTeamsAccount(ctx, team, teamType)
+}
+func (l loggingMiddleware) UpdateTeamsAccount(ctx context.Context, team interface{}, teamID string) (id string, e0 error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "UpdateTeamsAccount"), zap.String("team", fmt.Sprintf("%v", team)),
+			zap.String("teamID", teamID), zap.Any("err", e0))
+	}()
+	return l.next.UpdateTeamsAccount(ctx, team, teamID)
+}
+func (l loggingMiddleware) DeleteTeamsAccount(ctx context.Context, teamID string) (id string, e0 error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "DeleteTeamsAccount"),
+			zap.String("teamID", teamID), zap.Any("err", e0))
+	}()
+	return l.next.DeleteTeamsAccount(ctx, teamID)
+}
+func (l loggingMiddleware) GetTeamsAccount(ctx context.Context, teamID string) (team interface{}, err error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "GetTeamsAccount"),zap.String("team", fmt.Sprintf("%v", team)),
+			zap.String("teamID", teamID), zap.Any("err", err))
+	}()
+	return l.next.GetTeamsAccount(ctx, teamID)
+}
+func (l loggingMiddleware) AddUserToTeamsAccount(ctx context.Context, teamID string, userID string) (id string, e0 error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "AddUserToTeamsAccount"),zap.String("userID", userID),
+			zap.String("teamID", teamID), zap.Any("err", e0))
+	}()
+	return l.next.AddUserToTeamsAccount(ctx, teamID, userID)
+}
+func (l loggingMiddleware) DeleteUserFromTeamsAccount(ctx context.Context, teamID string, userID string) (id string, e0 error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "DeleteUserFromTeamsAccount"),zap.String("userID", userID),
+			zap.String("teamID", teamID), zap.Any("err", e0))
+	}()
+	return l.next.DeleteUserFromTeamsAccount(ctx, teamID, userID)
+}
+func (l loggingMiddleware) CreateGroup(ctx context.Context, groupName string, groupType string, isPrivate bool) (id string, e0 error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "CreateGroup"),zap.String("groupName", groupName),
+			zap.String("groupType", groupType), zap.Bool("isPrivate", isPrivate),zap.Any("err", e0))
+	}()
+	return l.next.CreateGroup(ctx, groupName, groupType, isPrivate)
+}
+func (l loggingMiddleware) SubscribeToGroup(ctx context.Context, groupID string, ID string) (id string, e0 error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "SubscribeToGroup"),zap.String("id", ID),
+			zap.String("groupID", groupID), zap.Any("err", e0))
+	}()
+	return l.next.SubscribeToGroup(ctx, groupID, ID)
+}
+func (l loggingMiddleware) UnsubscribeFromGroup(ctx context.Context, groupID string, ID string) (id string, e0 error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "UnsubscribeFromGroup"),zap.String("id", ID),
+			zap.String("groupID", groupID), zap.Any("err", e0))
+	}()
+	return l.next.UnsubscribeFromGroup(ctx, groupID, ID)
+}
+func (l loggingMiddleware) DeleteGroup(ctx context.Context, groupID string, groupAdminID string) (id string, e0 error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "DeleteGroup"),zap.String("groupAdminID", groupAdminID),
+			zap.String("groupID", groupID), zap.Any("err", e0))
+	}()
+	return l.next.DeleteGroup(ctx, groupID, groupAdminID)
+}
+func (l loggingMiddleware) UpdateGroup(ctx context.Context, groupID string, newGroup interface{}) (group interface{}, err error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "UpdateGroup"),
+			zap.String("groupID", groupID), zap.String("newGroup", fmt.Sprintf("%v", newGroup)),
+			zap.String("Group", fmt.Sprintf("%v", group)),
+			zap.Any("err", err))
+	}()
+	return l.next.UpdateGroup(ctx, groupID, newGroup)
+}
+func (l loggingMiddleware) GetGroupByID(ctx context.Context, groupID string) (group interface{}, err error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "GetGroupByID"),
+			zap.String("group", fmt.Sprintf("%v", group)),
+			zap.String("groupID", groupID), zap.Any("err", err))
+	}()
+	return l.next.GetGroupByID(ctx, groupID)
+}
+func (l loggingMiddleware) GetGroupByName(ctx context.Context, groupName string) (group interface{}, err error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "GetGroupByName"),
+			zap.String("group", fmt.Sprintf("%v", group)),
+			zap.String("groupName", groupName), zap.Any("err", err))
+	}()
+	return l.next.GetGroupByName(ctx, groupName)
+}
+func (l loggingMiddleware) IsGroupPrivate(ctx context.Context, groupID string) (isPrivate bool, err error) {
+	defer func() {
+		l.logger.Info("method", zap.String("function", "IsGroupPrivate"),
+			zap.Bool("isPrivate", isPrivate),
+			zap.String("groupID", groupID), zap.Any("err", err))
+	}()
+	return l.next.IsGroupPrivate(ctx, groupID)
+}
---
+++ pkg/api/service/service.go
@@ -0,0 +1,208 @@
+package service
+
+import (
+	"context"
+	"errors"
+
+	sdetcd "github.com/go-kit/kit/sd/etcd"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	)
+
+type basicUsersService struct{}
+
+type ConnectionProperties struct {
+	maxRetryCount int
+}
+
+type UsersService interface {
+	// User accounts specific interfaces. It is important to note that
+	// users can be of three types: investor, startup member, and regular user.
+	// Startup members are automatically associated with a team/startup. Investor
+	// type users can be standalone or associated with an investor team account.
+	// Regular users are not tied to any team and are inherently standalone.
+	CreateUserAccount(ctx context.Context, user interface{}, userType string) (string, error)
+	UpdateUserAccount(ctx context.Context, user interface{}, id string) (string, error)
+	DeleteUserAccount(ctx context.Context, id string) (string, error)
+	GetUserAccount(ctx context.Context, id string) (user interface{}, err error)
+
+	// This presents interfaces tied to team accounts. Team accounts can be of two types:
+	// startup teams or investor teams. Such teams are comprised of different user types and
+	// fields detailing data specific to the team function.
+	CreateTeamsAccount(ctx context.Context, team interface{}, teamType string) (string, error)
+	UpdateTeamsAccount(ctx context.Context, team interface{}, teamID string) (string, error)
+	DeleteTeamsAccount(ctx context.Context, teamID string) (string, error)
+	GetTeamsAccount(ctx context.Context, teamID string) (team interface{}, err error)
+	AddUserToTeamsAccount(ctx context.Context, teamID string, userID string) (string, error)
+	DeleteUserFromTeamsAccount(ctx context.Context, teamID string, userID string) (string, error)
+
+	// This presents interfaces tied to the group mechanism. Users can create user groups/communities
+	// and post content within such communities. Additionally, groups can be public or private
+	// hence, user can subscribe and unsubscribe to content posted in such a group.
+	CreateGroup(ctx context.Context, groupName string, groupType string, isPrivate bool) (string, error)
+	SubscribeToGroup(ctx context.Context, groupID string, ID string /*teams or user*/) (string, error)
+	UnsubscribeFromGroup(ctx context.Context, groupID string, ID string /*teams or user*/) (string, error)
+	DeleteGroup(ctx context.Context, groupID string, groupAdminID string) (string, error)
+	UpdateGroup(ctx context.Context, groupID string, newGroup interface{}) (group interface{}, err error)
+	GetGroupByID(ctx context.Context, groupID string) (group interface{}, err error)
+	GetGroupByName(ctx context.Context, groupName string) (group interface{}, err error)
+	IsGroupPrivate(ctx context.Context, groupID string) (isPrivate bool, err error)
+}
+
+func (b *basicUsersService) CreateUserAccount(ctx context.Context, user interface{}, userType string) (id string, e0 error){
+	// TODO implement the business logic of UpdateUserAccount
+	return "",e0
+}
+
+func (b *basicUsersService) UpdateUserAccount(ctx context.Context, user interface{}, id string) (userID string, e0 error){
+	// TODO implement the business logic of UpdateUserAccount
+	return "", e0
+}
+func (b *basicUsersService) DeleteUserAccount(ctx context.Context, id string) (userID string, e0 error){
+	// TODO implement the business logic of DeleteUserAccount
+	return "",e0
+}
+func (b *basicUsersService) GetUserAccount(ctx context.Context, id string) (user interface{}, err error) {
+	// TODO implement the business logic of GetUserAccount
+	return user, err
+}
+func (b *basicUsersService) CreateTeamsAccount(ctx context.Context, team interface{}, teamType string) (id string, e0 error) {
+	// TODO implement the business logic of CreateTeamsAccount
+	return "", e0
+}
+func (b *basicUsersService) UpdateTeamsAccount(ctx context.Context, team interface{}, teamID string) (id string, e0 error) {
+	// TODO implement the business logic of UpdateTeamsAccount
+	return "",e0
+}
+func (b *basicUsersService) DeleteTeamsAccount(ctx context.Context, teamID string) (id string, e0 error) {
+	// TODO implement the business logic of DeleteTeamsAccount
+	return "", e0
+}
+func (b *basicUsersService) GetTeamsAccount(ctx context.Context, teamID string) (team interface{}, err error) {
+	// TODO implement the business logic of GetTeamsAccount
+	return team, err
+}
+func (b *basicUsersService) AddUserToTeamsAccount(ctx context.Context, teamID string, userID string) (id string, e0 error) {
+	// TODO implement the business logic of AddUserToTeamsAccount
+	return "",e0
+}
+func (b *basicUsersService) DeleteUserFromTeamsAccount(ctx context.Context, teamID string, userID string) (id string, e0 error) {
+	// TODO implement the business logic of DeleteUserFromTeamsAccount
+	return "", e0
+}
+func (b *basicUsersService) CreateGroup(ctx context.Context, groupName string, groupType string, isPrivate bool) (id string, e0 error) {
+	// TODO implement the business logic of CreateGroup
+	return "", e0
+}
+func (b *basicUsersService) SubscribeToGroup(ctx context.Context, groupID string, ID string) (id string, e0 error) {
+	// TODO implement the business logic of SubscribeToGroup
+	return "", e0
+}
+func (b *basicUsersService) UnsubscribeFromGroup(ctx context.Context, groupID string, ID string) (id string, e0 error) {
+	// TODO implement the business logic of UnsubscribeFromGroup
+	return "", e0
+}
+func (b *basicUsersService) DeleteGroup(ctx context.Context, groupID string, groupAdminID string) (id string, e0 error) {
+	// TODO implement the business logic of DeleteGroup
+	return "", e0
+}
+func (b *basicUsersService) UpdateGroup(ctx context.Context, groupID string, newGroup interface{}) (group interface{}, err error) {
+	// TODO implement the business logic of UpdateGroup
+	return group, err
+}
+func (b *basicUsersService) GetGroupByID(ctx context.Context, groupID string) (group interface{}, err error) {
+	// TODO implement the business logic of GetGroupByID
+	return group, err
+}
+func (b *basicUsersService) GetGroupByName(ctx context.Context, groupName string) (group interface{}, err error) {
+	// TODO implement the business logic of GetGroupByName
+	return group, err
+}
+func (b *basicUsersService) IsGroupPrivate(ctx context.Context, groupID string) (isPrivate bool, err error) {
+	// TODO implement the business logic of IsGroupPrivate
+	return isPrivate, err
+}
+
+// NewBasicUsersService returns a naive, stateless implementation of UsersService.
+func NewBasicUsersService() UsersService {
+	return &basicUsersService{}
+}
+
+// New returns a UsersService with all of the expected middleware wired in.
+func New(middleware []Middleware) UsersService {
+	var svc UsersService = NewBasicUsersService()
+	for _, m := range middleware {
+		svc = m(svc)
+	}
+	return svc
+}
+
+func connectToServices(Logger *zap.Logger, connectionProperties ConnectionProperties, servicePrefixes... string /* ex. "/services/notificator/"*/) error {
+	var etcdServer = "http://etcd:2379"
+	client, err := sdetcd.NewClient(context.Background(), []string{etcdServer}, sdetcd.ClientOptions{})
+	if err != nil {
+		Logger.Error("unable to connect to etcd: %s", zap.Error(err))
+		// TODO: Return option must be present
+		return err
+	}
+
+	// Make sure service Prefix slice is not empty and attempt connection to all entries
+	// present in the slice
+	for _, servicePrefix := range servicePrefixes{
+		// Obtain number of service instances presently defined in the docker config.
+		entries, err := client.GetEntries(servicePrefix)
+
+		// perform error checking.
+		if err != nil || len(entries) == 0 {
+			Logger.Error("unable to get prefix entries: %s", zap.Error(err))
+			return err
+		}
+
+		// connect to a service instance
+		_, err = grpc.Dial(entries[0], grpc.WithInsecure())
+
+		if err != nil {
+			Logger.Error("unable to connect to notificator: %s. attempting retry", zap.Error(err))
+
+			err = retryConnection(entries, 0, connectionProperties.maxRetryCount,Logger)
+
+			if err != nil {
+				Logger.Error("retry count exceeded. unable to connect to service", zap.Error(err))
+				return err
+			}
+			return err
+		}
+
+		Logger.Info("successfully connected to service", zap.String("ServiceName", servicePrefix))
+	}
+	return nil
+}
+
+func retryConnection(serviceInstances []string, selectedInstanceIdx int, maxRetryCount int, Logger *zap.Logger) error{
+	retryCount := 0
+	lastInstanceIdx := len(serviceInstances) - 1
+	for retryCount <= maxRetryCount {
+		// attempt connection retry to an instances starting from the last instance
+		_ , err := grpc.Dial(serviceInstances[lastInstanceIdx], grpc.WithInsecure())
+
+		lastInstanceIdx--
+
+		if lastInstanceIdx != -1 {
+			lastInstanceIdx = len(serviceInstances) -1
+		}
+
+		// add error handling check.
+		if err != nil {
+			Logger.Error("unable to connect to notificator: %s", zap.Error(err))
+			retryCount++
+		} else {
+				return nil
+		}
+
+	}
+
+	return errors.New("unable to connect to service due to exceeded retry count")
+}
+
+
+
