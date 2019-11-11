9398513d8efc3f76cacf1189d96389f4b378937f
LensPlatform <iinvestplatform@gmail.com>
2019-11-11 00:17:18 -0800

    Added sql mappings

--- pkg/database/models.go
+++ pkg/database/models.go
@@ -13,104 +13,104 @@ type JsonEmbeddable struct {}
 // User represents a single user profile
 // ID should always be globally unique
 type User struct {
-	ID string `json:"id" validate:"-" db:"id"`
-	FirstName string `json:"first_name" validate:"required" db:"firstname"`
-	LastName string `json:"last_name" validate:"required" db:"lastname"`
-	UserName string `json:"user_name" validate:"required" db:"username"`
-	Gender string `json:"gender" validate:"-" db:"gender"`
-	Languages string `json:"Languages" validate:"-" db:"languages"`
-	Email string `json:"email" validate:"required,email" db:"email"`
-	PassWord string `json:"password" validate:"required,gte=8,lte=20" db:"password"`
-	PassWordConfirmed string `json:"password_confirmed" validate:"required,gte=8,lte=20" db:"passwordconf"`
-	Age int `json:"age" validate:"gte=0,lte=120" db:"age"`
-	BirthDate string `json:"birth_date" validate:"required" db:"birthdate"`
-	PhoneNumber string `json:"phone_number" validate:"required" db:"phonenumber"`
-	Addresses Address `json:"location" validate:"-" db:"address"`
-	Bio string `json:"bio" validate:"required" db:"bio"`
-	EducationalExperience Education `json:"education" validate:"-" db:"education"`
-	UserInterests Interests `json:"interests" validate:"-" db:"interests"`
-	Headline string `json:"headline" validate:"max=30" db:"headline"`
-	UserSubscriptions Subscriptions `json:"subscriptions" validate:"-" db:"subscriptions"`
-	Intent string `json:"intent" validate:"required" db:"intent"`
-	Skills Skillset `json:"skillset" validate:"-" db:"skills"`
-	AssociatedTeams []string `json:"associated_teams" validate:"-" db:"associated_teams"`
+	ID string `json:"id" validate:"-" sql:"id"`
+	FirstName string `json:"first_name" validate:"required" sql:"firstname"`
+	LastName string `json:"last_name" validate:"required" sql:"lastname"`
+	UserName string `json:"user_name" validate:"required" sql:"username"`
+	Gender string `json:"gender" validate:"-" sql:"gender"`
+	Languages string `json:"Languages" validate:"-" sql:"languages"`
+	Email string `json:"email" validate:"required,email" sql:"email"`
+	PassWord string `json:"password" validate:"required,gte=8,lte=20" sql:"password"`
+	PassWordConfirmed string `json:"password_confirmed" validate:"required,gte=8,lte=20" sql:"passwordconf"`
+	Age int `json:"age" validate:"gte=0,lte=120" sql:"age"`
+	BirthDate string `json:"birth_date" validate:"required" sql:"birthdate"`
+	PhoneNumber string `json:"phone_number" validate:"required" sql:"phonenumber"`
+	Addresses Address `json:"location" validate:"-" sql:"address"`
+	Bio string `json:"bio" validate:"required" sql:"bio"`
+	EducationalExperience Education `json:"education" validate:"-" sql:"education"`
+	UserInterests Interests `json:"interests" validate:"-" sql:"interests"`
+	Headline string `json:"headline" validate:"max=30" sql:"headline"`
+	UserSubscriptions Subscriptions `json:"subscriptions" validate:"-" sql:"subscriptions"`
+	Intent string `json:"intent" validate:"required" sql:"intent"`
+	Skills Skillset `json:"skillset" validate:"-" sql:"skills"`
+	AssociatedTeams []string `json:"associated_teams" validate:"-" sql:"associated_teams"`
 }
 
 type Team struct{
-	Name string `json:"name" validate:"required"` // team name
-	Type string `json:"type" validate:"required"` // investor or startup team
-	Overview string `json:"overview" validate:"required"` // about the team
-	IndustryOfInterest string `json:"industry" validate:"required"` // industry of interest
-	FoundedDate date.Date `json:"founded_date" validate:"required"`
-	Founders []TeamMember `json:"founder" validate:"required"`
-	NumberOfEmployees int `json:"number_of_employees" validate:"required"` // size of team
-	Headquarters string `json:"headquarters" validate:"-"`
-	Interests string `json:"interests" validate:"-"`
-	TeamMembers []TeamMember `json:"team_members" validate:"-"`
-	Advisors []TeamMember `json:"advisors" validate:"-"`
-	SocialMedia SocialMedia `json:"social_media" validate:"-"`
-	Contact Contact `json:"contact" validate:"-"`
+	JsonEmbeddable
+	Name string `json:"name" validate:"required" sql:"name"` // team name
+	Type string `json:"type" validate:"required" sql:"type"` // investor or startup team
+	Overview string `json:"overview" validate:"required" sql:"overview"` // about the team
+	IndustryOfInterest string `json:"industry" validate:"required" sql:"industryofinterest"` // industry of interest
+	FoundedDate date.Date `json:"founded_date" validate:"required" sql:"foundeddate"`
+	Founders []TeamMember `json:"founder" validate:"required" sql:"founders"`
+	NumberOfEmployees int `json:"number_of_employees" validate:"required" sql:"numberofemployees"` // size of team
+	Headquarters string `json:"headquarters" validate:"-" sql:"headquarters"`
+	Interests string `json:"interests" validate:"-" sql:"interests"`
+	TeamMembers []TeamMember `json:"team_members" validate:"-" sql:"teammembers"`
+	Advisors []TeamMember `json:"advisors" validate:"-" sql:"advisors"`
+	SocialMedia SocialMedia `json:"social_media" validate:"-" sql:"socialmedia"`
+	Contact Contact `json:"contact" validate:"-" sql:"contact"`
 }
 
 type Startup struct {
 	Team
-	Funds Funding `json:"funding" validate:"-"`
-	CompanyDetails Details `json:"company_details" validate:"-"`
+	Funding Funding `json:"funding" validate:"-" sql:"funding"`
+	CompanyDetails Details `json:"company_details" validate:"-" sql:"companydetails"`
 }
 
 type Investor struct{
 	Team
-	InvestorType string `json:"investor_type" validate:"-"`
-	InvestmentStage string `json:"investment_stage" validate:"-"`
-	NumberOfExits int `json:"number_of_exits" validate:"-"`
-	NumberOfinvestments int `json:"number_of_investments" validate:"-"`
-	NumberOfFunds int `json:"number_of_funds" validate:"-"`
-	TotalFundsRaised int `json:"total_funds_raised" validate:"-"`
+	InvestorType string `json:"investor_type" validate:"-" sql:"investortype"`
+	InvestmentStage string `json:"investment_stage" validate:"-" sql:"investmentstage"`
+	NumberOfExits int `json:"number_of_exits" validate:"-" sql:"numberofexits"`
+	NumberOfinvestments int `json:"number_of_investments" validate:"-" sql:"numberofinvestments"`
+	NumberOfFunds int `json:"number_of_funds" validate:"-" sql:"numberoffunds"`
 }
 
 type Funding struct {
 	JsonEmbeddable
-	TotalFunding int `json:"total_funding" validate:"required"`
-	LatestRound string `json:"latest_round" validate:"required"`
-	LatestRoundDate string `json:"latest_round_date" validate:"required"`
-	LatestRoundFunding int `json:"latest_round_funding" validate:"required"`
-	Investors []string `json:"investors" validate:"-"`
-	InvestorType string `json:"investor_type" validate:"-"` //Accelerator
-	InvestmentStage string `json:"investment_stage" validate:"-"` // Debt, Early Stage Venture, Seed
+	TotalFunding int `json:"total_funding" validate:"required" sql:"totalfunding"`
+	LatestRound string `json:"latest_round" validate:"required" sql:"latestround"`
+	LatestRoundDate string `json:"latest_round_date" validate:"required" sql:"latestrounddate"`
+	LatestRoundFunding int `json:"latest_round_funding" validate:"required" sql:"latestroundfunding"`
+	Investors []string `json:"investors" validate:"-" sql:"investors"`
+	InvestorType string `json:"investor_type" validate:"-" sql:"investortype"` //Accelerator
+	InvestmentStage string `json:"investment_stage" validate:"-" sql:"investmentstage"` // Debt, Early Stage Venture, Seed
 }
 
 type Details struct {
 	JsonEmbeddable
-	IPOStatus string `json:"ipo_status" validate:"-"`
-	CompanyType string `json:"company_type" validate:"-"` // for-profit
+	IPOStatus string `json:"ipo_status" validate:"-" sql:"ipostatus"`
+	CompanyType string `json:"company_type" validate:"-" sql:"companytype"` // for-profit
 }
 
 type TeamMember struct {
 	JsonEmbeddable
-	ID string `json:"ID" validate:"required"`
-	Name string `json:"name" validate:"required"`
-	Status string `json:"status" validate:"required"` // co-founder
+	ID string `json:"ID" validate:"required" sql:"id"`
+	Name string `json:"name" validate:"required" sql:"name"`
+	Status string `json:"status" validate:"required" sql:"status"` // co-founder
 }
 
 type Contact struct {
 	JsonEmbeddable
-	Email string `json:"email" validate:"required"`
-	PhoneNumber string `json:"phonenumber" validated:"required"`
+	Email string `json:"email" validate:"required" sql:"email"`
+	PhoneNumber string `json:"phonenumber" validated:"required" sql:"phonenumber"`
 }
 
 type SocialMedia struct{
 	JsonEmbeddable
-	Website string `json:"website" validate:"-"`
-	Facebook string `json:"facebook" validate:"-"`
-	Twitter string `json:"twitter" validate:"-"`
-	LinkedIn string `json:"linkedIn" validate:""`
+	Website string `json:"website" validate:"-" sql:"website"`
+	Facebook string `json:"facebook" validate:"-" sql:"facebook"`
+	Twitter string `json:"twitter" validate:"-" sql:"twitter"`
+	LinkedIn string `json:"linkedIn" validate:"-" sql:"linkedin"`
 }
 
 type Address struct {
 	JsonEmbeddable
-	City string `json:"city" validate:"required"`
-	State string `json:"state" validate:"required"`
-	Country string `json:"country" validate:"required"`
+	City string `json:"city" validate:"required" sql:"city"`
+	State string `json:"state" validate:"required" sql:"state"`
+	Country string `json:"country" validate:"required" sql:"country"`
 }
 
 type Education struct{
